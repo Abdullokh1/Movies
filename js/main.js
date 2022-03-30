b167f47c34b38a9b54791b9bffc46b6e2b6c9b33
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-03-30 08:59:37 +0500

    slider

--- js/main.js
+++ js/main.js
@@ -16,71 +16,11 @@ let Result = document.getElementById('FilterResult');
 
 
 
-
-
-
 // FIRST SECTION ADDED -----
 
 
 let id = 0;
-movies.forEach((item, index) =>{
-  if (index > 5 && index < 14){
-    let li = document.createElement('li');
-    ElList.style.flexWrap = 'nowrap';
-    li.className = 'list__item col-3';
-    li.innerHTML = `
-    <div class="item-inner">
-    <div class="img-wrapper">
-      <img class="card-img" src="${item.youtubePoster}" alt="card-img-1">
-    </div>
-    <div class="series-wrapper d-flex justify-content-between align-items-center">
-      <p class="series-text m-0">Tv series</p>
-      <button class="heart-btn" onclick = "Heart('${item.imdbId}')"><i class='bx bxs-heart'></i></button>
-    </div>
-    <div class='d-flex justify-content-between'>
-    <span class="card-usa">${item.language} <span>${item.year}</span> - Current</span>
 
-    <button class="btn modal-btn mt-2"  data-bs-toggle="modal" data-bs-target="#exampleModal${id}">More</button>
-      <div class="modal fade" id="exampleModal${id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Movie Description</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-            <div class='d-flex'>
-              <img class="card-img modal-img me-5" src="${item.youtubePoster}" alt="card-img-1">
-              ${item.summary}
-            </div>
-             <img src="img/card-small-img.svg" alt="card-img" width="35" height="17">
-             <span class="ms-2">${item.runtime}</span>
-             <i class='bx bxs-star'></i>
-             <span class='me-3'>${item.imdbRating}</span>
-             <p class="card-usa">${item.categories}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <h3 class="card-stranger">${item.title}</h3>
-    <div class="d-flex justify-content-between ">
-      <div>
-        <img src="img/card-small-img.svg" alt="card-img" width="35" height="17">
-        <span class="ms-2">${item.runtime}</span>
-      </div>
-      <div>
-        <i class='bx bxs-star'></i>
-        <span>${item.imdbRating}</span>
-      </div>
-      </div>
-      <p class="card-usa">${item.categories}</p>
-  </div>
-    `;
-    id++;
-    ElList.appendChild(li);
-  }
-})
 
 
 // SECOND SECTION ADDED ----
@@ -226,14 +166,7 @@ temp.forEach((item, index) =>{
 
 // WISHLIST PART --------
 
-let heart = document.querySelectorAll('.heart-btn');
-heart.forEach(item =>{
-   item.addEventListener('click', (e) =>{
-     e.target.style.color = '#be123c';
-
-   })
-})
-
+let wish = [];
 function Heart(id){
   movies.forEach((item,index) =>{
     let li = document.createElement('li');
@@ -266,8 +199,9 @@ function Heart(id){
     <p class="card-usa">${movies[index].categories}</p>
     </div>
     `;
-      Wishlist.appendChild(li);
-    }
+    Wishlist.appendChild(li);
+    
+  }
 })
 
 
@@ -286,7 +220,7 @@ removeBtn.forEach(item => {
 
 
 movies.forEach((item,index) =>{
-  if (index < 10){
+  if (index < 20){
     let cat = item.categories;
     cat.forEach(el =>{
       let option = document.createElement('option');
@@ -304,8 +238,10 @@ FilterForm.addEventListener('submit', (e) =>{
  let value = Search.value.toLowerCase();
  let from = YearFrom.value;
  let to = YearTo.value;
+ let categ = select.value;
  
  Result.innerHTML = '';
+
  movies.forEach(el =>{
    let title = el.title.toLowerCase();
    if (title.indexOf(value) != -1 && el.year >= from && el.year <= to){
@@ -364,10 +300,6 @@ FilterForm.addEventListener('submit', (e) =>{
       Result.appendChild(li);
     }
   })
-  Search.value = '';
-  YearFrom.value = '';
-  YearTo.value = '';
-
 })
 
 
@@ -447,9 +379,6 @@ Form.addEventListener('submit', (e) =>{
         id++;
     
         modalUl.appendChild(li);
-
-        
-        
         modalUl.style.top = '80px';
         modalUl.style.transition = 'all 0.5s ease';
         
@@ -459,7 +388,6 @@ Form.addEventListener('submit', (e) =>{
           li.innerHTML = '';
           modalUl.style.transition = 'none';
         })
-
     }
     HeroSearch.value = '';
   })
@@ -469,154 +397,92 @@ Form.addEventListener('submit', (e) =>{
 
 // SLIDER PART ------
 
-let num = 0;
-let num2 = 0;
-let num3 = 0;
+let listArray = [];
 
-RightSlide.forEach((item, index) =>{
+  movies.forEach((item, index) =>{
+    if (index < 8){
+      let li = document.createElement('li');
+      ElList.style.flexWrap = 'nowrap';
+      li.className = 'list__item col-3';
+      li.innerHTML = `
+      <div class="item-inner">
+      <div class="img-wrapper">
+        <img class="card-img" src="${item.youtubePoster}" alt="card-img-1">
+        </div>
+      <div class="series-wrapper d-flex justify-content-between align-items-center">
+        <p class="series-text m-0">Tv series</p>
+        <button class="heart-btn" onclick = "Heart('${item.imdbId}')"><i class='bx bxs-heart'></i></button>
+      </div>
+      <div class='d-flex justify-content-between'>
+      <span class="card-usa">${item.language} <span>${item.year}</span> - Current</span>
+        <button type="button" class="btn modal-btn mt-2" data-bs-toggle="modal" data-bs-target="#exampleModal${id}">More</button>
+        
+        <div class="modal fade" id="exampleModal${id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+          <div class="modal-content">
+          <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Movie Description</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+          <div class='d-flex'>
+           <img class="card-img modal-img me-5" src="${item.youtubePoster}" alt="card-img-1">
+            ${item.summary}
+           </div>
+           <img src="img/card-small-img.svg" alt="card-img" width="35" height="17">
+           <span class="ms-2">${item.runtime}</span>
+           <i class='bx bxs-star'></i>
+           <span class='me-3'>${item.imdbRating}</span>
+           <p class="card-usa">${item.categories}</p>
+           
+          </div>
+         </div>
+        </div>
+        </div>
+       </div>
+      <h3 class="card-stranger">${item.title}</h3>
+      <div class="d-flex justify-content-between ">
+        <div>
+          <img src="img/card-small-img.svg" alt="card-img" width="35" height="17">
+          <span class="ms-2">${item.runtime}</span>
+        </div>
+        <div>
+          <i class='bx bxs-star'></i>
+          <span>${item.imdbRating}</span>
+        </div>
+        </div>
+      <p class="card-usa">${item.categories}</p>
+      </div>
+      `;
+      id++;
+      listArray.push(li);
+    }
+  })
 
-  if (index == 0){
-    item.addEventListener('click', ()=>{
-        ElList.style.transition = 'transform 0.3s ease';
-        num++;
-        if (num == 1){
-          ElList.style.transform = 'translateX(-25%)';
-        }
-        else if  (num == 2){
-          ElList.style.transform = 'translateX(-50%)';
-        }
-        else if  (num == 3){
-          ElList.style.transform = 'translateX(-75%)';
-        }
-        else if  (num == 4){
-          ElList.style.transform = 'translateX(-100%)';
-        }
-        else{
-          ElList.style.transform = 'translate(0)';
-          num = 0;
-        }
+  function addItem(){
+    listArray.forEach(el =>{
+      ElList.appendChild(el);
     })
-
   }
-  
-  if (index == 1){
-    item.addEventListener('click', ()=>{
-        Arrival.style.transition = 'transform 0.3s ease';
-        num2++;
-        if (num2 == 1){
-          Arrival.style.transform = 'translateX(-25%)';
-        }
-        else if  (num2 == 2){
-          Arrival.style.transform = 'translateX(-50%)';
-        }
-        else if  (num2 == 3){
-          Arrival.style.transform = 'translateX(-75%)';
-        }
-        else if  (num2 == 4){
-          Arrival.style.transform = 'translateX(-100%)';
-        }
-        else{
-          Arrival.style.transform = 'translate(0)';
-          num2 = 0;
-        }
-    })
-  }
+  addItem();
 
-    if (index == 2){
-      item.addEventListener('click', ()=>{
-          Exclusive.style.transition = 'transform 0.3s ease';
-          num3++;
-          if (num3 == 1){
-            Exclusive.style.transform = 'translateX(-25%)';
-          }
-          else if  (num3 == 2){
-            Exclusive.style.transform = 'translateX(-50%)';
-          }
-          else if  (num3 == 3){
-            Exclusive.style.transform = 'translateX(-75%)';
-          }
-          else if  (num3 == 4){
-            Exclusive.style.transform = 'translateX(-100%)';
-          }
-          else{
-            Exclusive.style.transform = 'translate(0)';
-            num3 = 0;
-          }
-      })
-}
+ 
+
+
+let firstCount = 0;
+
+RightSlide.forEach((item,index) =>{
+  item.addEventListener('click', ()=>{
+    if (index == 0){
+      let c = listArray[firstCount];
+      listArray[firstCount] = listArray[firstCount +1];
+      listArray[listArray.length] = c;
+      firstCount++;
+      addItem();
+    }
+  })
 })
 
-LeftSlide.forEach((item, index) =>{
-  if (index == 0){
-    item.addEventListener('click', ()=>{
-        ElList.style.transition = 'transform 0.3s ease';
-        num--;
-        if (num == 1){
-          ElList.style.transform = 'translateX(-25%)';
-        }
-        else if  (num == 2){
-          ElList.style.transform = 'translateX(-50%)';
-        }
-        else if  (num == 3){
-          ElList.style.transform = 'translateX(-75%)';
-        }
-        else if  (num == 4){
-          ElList.style.transform = 'translateX(-100%)';
-        }
-        else{
-          ElList.style.transform = 'translate(0)';
-          num = 0;
-        }
-    })
-  }
-
-
-  if (index == 1){
-    item.addEventListener('click', ()=>{
-        Arrival.style.transition = 'transform 0.3s ease';
-        num2--;
-        if (num2 == 1){
-          Arrival.style.transform = 'translateX(-25%)';
-        }
-        else if  (num2 == 2){
-          Arrival.style.transform = 'translateX(-50%)';
-        }
-        else if  (num2 == 3){
-          Arrival.style.transform = 'translateX(-75%)';
-        }
-        else if  (num2 == 4){
-          Arrival.style.transform = 'translateX(-100%)';
-        }
-        else{
-          Arrival.style.transform = 'translate(0)';
-          num2 = 0;
-        }
-    })
-  }
-
-  if (index == 2){
-    item.addEventListener('click', ()=>{
-        Exclusive.style.transition = 'transform 0.3s ease';
-        num3--;
-        if (num3 == 1){
-          Exclusive.style.transform = 'translateX(-25%)';
-        }
-        else if  (num3 == 2){
-          Exclusive.style.transform = 'translateX(-50%)';
-        }
-        else if  (num3 == 3){
-          Exclusive.style.transform = 'translateX(-75%)';
-        }
-        else if  (num3 == 4){
-          Exclusive.style.transform = 'translateX(-100%)';
-        }
-        else{
-          Exclusive.style.transform = 'translate(0)';
-          num3 = 0;
-        }
-    })
-  }
-})
 
 movies.forEach((item, index) =>{
   if (index > 200 && index < 229){
@@ -675,4 +541,14 @@ movies.forEach((item, index) =>{
     All.appendChild(li);
   }
   
+})
+
+
+
+
+let heart = document.querySelectorAll('.heart-btn');
+heart.forEach(item =>{
+   item.addEventListener('click', (e) =>{
+     e.target.style.color = '#be123c';
+   })
 })
